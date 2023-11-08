58b5d25501b54fd7cec23feb0feded236ba5c9f3
전수빈 <soobin08011812@gmail.com>
2023-11-08 13:02:52 +0900

    Feat: postCard - 답변 상태 prop 추가 및 elintrc 추가

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,10 @@
 import InputField from '../../components/common/InputField'
-import PostCard from '../../components/postCard/PostCard'
 
 const Home = () => {
   return (
     <div>
       <h1>home</h1>
       <InputField />
-      <PostCard />
     </div>
   )
 }
