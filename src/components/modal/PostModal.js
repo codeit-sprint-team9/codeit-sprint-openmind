49d6922dec71170f16704699e87d183acb7a2418
전수빈 <soobin08011812@gmail.com>
2023-11-08 20:53:52 +0900

    Feat: post - 모달 추가

---
+++ src/components/modal/PostModal.js
@@ -0,0 +1,137 @@
+import styled from 'styled-components'
+import MessageIcon from '../../asset/postModal/img_message.svg'
+import CloseIcon from '../../asset/postModal/img_close.svg'
+import InputTextArea from '../common/InputTextArea'
+import Button from '../common/Button'
+import { device } from '../styles'
+
+const PostModal = ({ setIsOpened }) => {
+  return (
+    <Overlay>
+      <OuterModalContainer onClick={() => setIsOpened(false)} />
+
+      <ModalMainContainer>
+        <TitleContainer>
+          <img src={MessageIcon} className="messageIcon" alt="messageIcon" />
+          <div className="title">질문을 작성하세요</div>
+          <img
+            src={CloseIcon}
+            className="closeIcon"
+            alt="closeIcon"
+            onClick={() => setIsOpened(false)}
+          />
+        </TitleContainer>
+
+        <ContentContainer>
+          <div className="userContainer">
+            <div className="to">To.</div>
+            <img className="userIcon" />
+            <div className="userName">아초는고양이</div>
+          </div>
+
+          <InputTextArea />
+
+          <Button brown={true} text="질문 보내기" />
+        </ContentContainer>
+      </ModalMainContainer>
+    </Overlay>
+  )
+}
+
+export default PostModal
+
+export const Overlay = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  z-index: 100;
+`
+
+export const OuterModalContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  opacity: 0.4;
+`
+export const ModalMainContainer = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  width: 61.2rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  border-radius: 2.4rem;
+  border: 1px solid #dee2e6;
+  padding: 4rem 4rem 7rem;
+  box-sizing: border-box;
+  align-items: center;
+  justify-content: center;
+  gap: 2.4rem;
+  @media all and ${device.mobile} {
+    width: 32.7rem;
+    padding: 2.4rem;
+  }
+`
+export const TitleContainer = styled.div`
+  display: flex;
+  width: 100%;
+  margin-bottom: 4rem;
+
+  .messageIcon {
+    margin-right: 0.8rem;
+  }
+
+  .title {
+    color: var(--gray-60);
+    font-size: 2.4rem;
+    font-weight: 400;
+    line-height: 3rem;
+    @media all and ${device.mobile} {
+      font-size: 2rem;
+      line-height: 2.5rem;
+    }
+  }
+
+  .closeIcon {
+    margin-left: auto;
+    cursor: pointer;
+    @media all and ${device.mobile} {
+      width: 2.2rem;
+      height: 2.2rem;
+    }
+  }
+`
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  width: 100%;
+
+  .userContainer {
+    display: flex;
+    gap: 0.4rem;
+    align-items: center;
+    color: var(--gray-60);
+    font-weight: 400;
+    margin-bottom: 1.2rem;
+
+    .to {
+      font-size: 1.8rem;
+      line-height: 2.4rem;
+    }
+
+    .userIcon {
+      width: 2.8rem;
+      height: 2.8rem;
+    }
+
+    .userName {
+      font-size: 1.6rem;
+      line-height: 2.2rem;
+    }
+  }
+`
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,25 +8,31 @@ import InputField from '../../components/common/InputField'
 import InputTextArea from '../../components/common/InputTextArea'
 import Reaction from '../../components/common/Reaction'
 import Toast from '../../components/common/Toast'
+import PostModal from '../../components/modal/PostModal'
 
 const Home = () => {
   const [isValue, setIsValue] = useState(false)
+  const [isOpened, setIsOpened] = useState(false)
   return (
-    <div>
-      <h1>home</h1>
-      <InputField isValue={isValue} setIsValue={setIsValue} />
-      <InputTextArea isValue={isValue} setIsValue={setIsValue} />
-      <Badge isAnswered={true} />
-      <Badge isAnswered={false} />
-      <FloatingButton />
-      <Toast />
-      <Reaction like="5" disLike="2" />
-      <Reaction />
-      <Dropdown />
-      <Edit />
-      <Button brown isValue={isValue} />
-      <Button />
-    </div>
+    <>
+      <div>
+        <h1>home</h1>
+        <InputField isValue={isValue} setIsValue={setIsValue} />
+        <InputTextArea isValue={isValue} setIsValue={setIsValue} />
+        <Badge isAnswered={true} />
+        <Badge isAnswered={false} />
+        <FloatingButton />
+        <Toast />
+        <Reaction like="5" disLike="2" />
+        <Reaction />
+        <Dropdown />
+        <Edit />
+        <Button brown isValue={isValue} />
+        <Button />
+        <div onClick={() => setIsOpened(!isOpened)}>ok</div>
+      </div>
+      {isOpened && <PostModal setIsOpened={setIsOpened} />}
+    </>
   )
 }
 
