761d52da4e3a35e1036662d4643e1ffdf23d39a9
전수빈 <soobin08011812@gmail.com>
2023-11-16 02:47:14 +0900

    Delete: page404, page500 삭제

--- src/pages/error/Page404.js
+++
@@ -1,5 +0,0 @@
-const Page404 = () => {
-  return <div>404 페이지입니다.</div>;
-};
-
-export default Page404;
--- src/pages/error/Page500.js
+++
@@ -1,5 +0,0 @@
-const Page500 = () => {
-  return <div>500 페이지입니다.</div>;
-};
-
-export default Page500;
