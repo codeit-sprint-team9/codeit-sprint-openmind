04bf86246c1c974e1a31d8233459d0ae73445ace
Accept77 <accept_agreed@kakao.com>
2023-11-16 15:53:56 +0900

    fix: delete button

--- src/components/post/PostDeleteButton.js
+++ src/components/post/PostDeleteButton.js
@@ -7,7 +7,7 @@ const FloatingButtonStyledComponent = styled.div`
   padding: 1.2rem 2.4rem;
   border-radius: 20rem;
   background: ${({ $theme }) =>
-    $theme ? 'var(--brown-40);' : 'var(--gray-50);'}
+    $theme === 'light' ? 'var(--brown-40);' : 'var(--gray-50);'}
   box-shadow: 0 0.4rem 0.4rem 0 rgba(0, 0, 0, 0.25);
   color: var(--gray-10, #fff);
   font-size: 1.5rem;
